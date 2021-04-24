758be03bdc97b65be0c5f9519024183330db7fea
PilipenkoVA <78660567+PilipenkoVA@users.noreply.github.com>
2021-04-24 15:45:13 +0300

    Delete Lesson_3 directory

--- Lesson_3/Client/src/main/java/ru/client/CallbackArgs.java
+++
@@ -1,5 +0,0 @@
-package ru.client;
-
-public interface CallbackArgs {
-    void callback(Object... args);
-}
--- Lesson_3/Server/src/main/java/ru/server/AuthHandler.java
+++
@@ -1,7 +0,0 @@
-package ru.server;
-
-public interface AuthHandler {
-    void start();
-    String getNickByLoginPass(String login, String pass);
-    void stop();
-}
