ea63deedda6adf6426d8080ae8ae6bc47be78f5b
Vitaliy <Pilipenko.va@live.ru>
2021-05-05 16:06:36 +0300

    Добавил Логирование на серверную сторону чата, с выводом информации

--- TextChat/Server/src/main/java/ru/server/Server.java
+++ TextChat/Server/src/main/java/ru/server/Server.java
@@ -6,34 +6,49 @@ import java.net.Socket;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.*;
 
 public class Server {
+    private static final Logger loger = Logger.getLogger(Server.class.getName());                // <-- создаем "Logger"
+
     private ServerSocket serverSocket;
     private Vector<ClientHandler> clients;
-    private ExecutorService service;                                                      // <-- добавил ExecutorService
+    private ExecutorService service;
 
     public Server() {
+        loger.setLevel(Level.ALL);                                                                      // <-- добавляем
+        Logger.getLogger("").getHandlers()[0].setLevel(Level.ALL);                                      // <-- добавляем
+
+        Handler handler = new ConsoleHandler();                                          // <-- создаем "ConsoleHandler"
+        handler.setLevel(Level.ALL);                                                                    // <-- добавляем
+        loger.log(Level.ALL,"Logger to the Server writer...");                                     // <-- добавляем
         try {
             // 1. запуск БД
             AutoService.connect();
-            service = Executors.newFixedThreadPool(1000);                           // <-- добавил 1000 потоков
+            service = Executors.newFixedThreadPool(1000);
             serverSocket = new ServerSocket(8689);
             clients = new Vector<ClientHandler>();
             System.out.println("Сервер запущен");
+            loger.info("Сервер запущен...");                                                  // <-- запись события
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Клиент "+socket.getInetAddress()+" пытается подключится");
-                new ClientHandler(this, socket, service);                                  // <-- добавил service
+                loger.info("\"Клиент "+socket.getInetAddress()+" пытается подключится...");   // <-- запись события
+                new ClientHandler(this, socket, service);
                 System.out.println("Клиент "+socket.getInetAddress()+" подключился");
+                loger.info("\"Клиент "+socket.getInetAddress()+" подключился...");            // <-- запись события
                 System.out.println("Ожидание нового клиента");
+                loger.info("Ожидание нового клиента...");                                     // <-- запись события
             }
         } catch (Exception e) {
             e.printStackTrace();
+            loger.warning("Exception...");                                                    // <-- запись события
         } finally {
             try {
                 serverSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
+                loger.warning("Exception...");                                                // <-- запись события
             }
             // 1. закрытие БД
             AutoService.disconnect();
@@ -45,7 +60,8 @@ public class Server {
             if (o.getNick().equals(nickname)) {
                 o.sendMessage(from.getNick()+" [Отправил для "+ nickname + "] Сообщение: " + msg);
                 from.sendMessage("Пользователю: " + nickname + " Сообщение: " + msg);
-                // записываем личные сообщения в истрорию
+                loger.info("Клиент: "+from.getNick()+" отправил личное сообщение: "+nickname);// <-- запись события
+                // записываем личные сообщения в историю
                 AutoService.addRecordToDB(from.getNick(),nickname,from.getNick() + ": [прислал вам личное сообщение]: " + msg);
                 return;
             }
@@ -54,13 +70,15 @@ public class Server {
     }
     public void broadcastMsg(ClientHandler client, String msg) {
         String outMsg = client.getNick() + ": " + msg;
+        loger.info("Клиент: "+client.getNick()+" отправил сообщение в общий чат");            // <-- запись события
         for (ClientHandler o : clients) {
-            // 4. если клиент в "Blacklist" то от его сообщения не получаем
+            // если клиент в "Blacklist" то от его сообщения не получаем
             if (!o.checkBlackList(client.getNick())) {
                 o.sendMessage(outMsg);
 
-                // 5.записываем все сообщения в БД
+                // записываем все сообщения в БД
                 AutoService.addRecordToDB(client.getNick(),o.getNick(),msg);
+
             }
         }
     }
